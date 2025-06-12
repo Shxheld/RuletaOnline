56575dc9958dea7572ef50b7f5a1e76c6abbbffd
Shxheld <113806664+Shxheld@users.noreply.github.com>
2025-06-12 19:41:32 -0300

    Update server.cjs

--- ruletaonline1/public/server.cjs
+++ ruletaonline1/public/server.cjs
@@ -22,6 +22,7 @@ app.get('/', (req, res) => {
 });
 
 // ==== BASE DE DATOS POSTGRES ====
+const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
